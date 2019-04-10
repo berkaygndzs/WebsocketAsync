313fcbba4c80cf4e7489bd3b32a11b7f5da8e25f
guopenglun <downstairs@163.com>
2019-04-10 12:51:01 +0800

    Create WebsocketAsync.js

---
+++ WebsocketAsync.js
@@ -0,0 +1,56 @@
+const uuidv4 = require('uuid/v4')
+
+class WebsocketAsync {
+  constructor(url) {
+   
+    this._promisePool = {}
+    this._url = url
+    this._connecting = false
+  }
+
+  open() {
+    return new Promise((resolve, reject) => {
+      if (this._connecting) return
+      this._connecting = true
+      this._websocket = new WebSocket(this._url)
+      this._websocket.onopen = (e) => {
+        console.log('ws connected.')
+        resolve(e)
+      }
+      this._websocket.onerror = (e) => {
+        console.log('ws error.')
+        reject(e)
+      }
+      this._websocket.onmessage = (e) => {
+        const json = JSON.parse(e.data)
+        const key = json.token
+        const req = this._promisePool[key]
+        req.resolve(json)
+        delete this._promisePool[key]
+      }
+    })
+  }
+
+  close() {
+    this._websocket.close()
+    this._connecting = false
+  }
+
+  async send(msg) {
+    if (this._websocket == null || this._websocket.readyState !== 1) {
+      this._connecting = false
+      await this.open()
+    }
+    msg.token = uuidv4()
+    return new Promise((resolve, reject) => {
+      this._promisePool[msg.token] = {
+        msg,
+        resolve,
+        reject
+      }
+      this._websocket.send(JSON.stringify(msg))
+    })
+  }
+}
+const wsa = new WebsocketAsync('ws://127.0.0.1:996')
+export default wsa
